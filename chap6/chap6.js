e16036756bcd62a337749c861f30b28fdef0e72a
orlinnkulu5-tech <orlinnkulu5@gmail.com>
2026-04-24 11:14:27 +0200

    Add files via upload
    chap5-chap7

---
+++ chap6/chap6.js
@@ -0,0 +1,62 @@
+
+        // var message ="Hello, world!";
+        // console.log(message);
+
+        // IF STATEMENTS
+        // var name = "orlin";
+        // console.log("Hello, " + name );
+        // if (name.length > 7) {
+        //     console.log("wow,you have a really long name!");
+        // } else {
+        //     console.log(" your name isn't very long.");
+        // }
+
+
+        // CHAINING IF ..ELSE STATEMENTS
+        var lemonChicken = false;
+        var beefWithBlackBean = false;
+        var sweetAndSourPork = false;
+
+        if (lemonChicken) {
+            console.log("Great! I'm having lemon chicken!");
+        } else if (beefWithBlackBean) {
+            console.log("I'm having the beef");
+        } else if (sweetAndSourPork) {
+            console.log("ok, I'll have the pork");
+        } else {
+            console.log("Well, I guess I'll just have rice then...");
+        } 
+var sheepcounted = 0;
+
+while ( sheepcounted < 10) {
+    console.log("I have counted " + sheepcounted +"sheep!")
+    sheepcounted++;
+}
+console.log("Zzzzzzzzzzzz");
+// FOR LOOPS ;
+
+for ( var sheepcounted = 0; sheepcounted++ < 10; sheepcounted++) {
+    console.log(" I have counted" + sheepcounted + "sheep") ;
+}
+console.log("Zzzzzzzzzzzz");
+var timesToSayhello = 3;
+for (var i = 0;i < timesToSayhello; i++) {
+    console.log("hello!");
+}
+var animals = ["lion","flamingo","Polar bear", "Boa constrictor"]
+for (var i = 0; i < animals.length; i++) {console.log("this zoo contains " + animals[i] +".");
+    
+}
+var name = "orlin";
+
+for(var i = 0; i < name.length; i++) {console.log("my name contains the letter " + name[i]+ ".");
+
+} 
+
+    
+
+    
+    
+
+
+
