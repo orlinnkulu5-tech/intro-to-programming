761ced008c676cbf1a8a761fa31fcb88feeff99a
orlinnkulu5-tech <orlinnkulu5@gmail.com>
2026-05-07 20:19:03 +0200

    chap3

---
+++ book2/chap3/chapter3.js
@@ -0,0 +1,11 @@
+var toName = "orlin"
+
+var fromName = "the Grammy award";
+
+var letterBody ="we are pleased to inform you that your song, 'can\'t stop coding !,' has been voted the best song of all time by the awarding committee.";
+
+document.write("Dear " + toName + ',<br><br>');
+document.write(letterBody + "<br><br>");
+document.write("sincerely, <br>");
+document.write(fromName);
+    
